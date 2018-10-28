1268d43c4226558e03ff298d284457738615daf1
ryo117n <ryonakagawa117@gmail.com>
2018-10-29 02:04:21 +0900

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_140528) do
+ActiveRecord::Schema.define(version: 2018_10_28_170251) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "comment"
+    t.string "text"
+    t.integer "cr_type"
+    t.integer "user_id"
+    t.integer "advertiser_id"
+    t.integer "media_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
